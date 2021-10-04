e81623d8dfa5802ea2893e23b0ecf2ed5811591a
Armando HG <armand0hernandezglz@gmail.com>
2021-10-04 10:39:09 -0500

    reiniciar valor del select

--- src/app/equipos/pages/selector-page/selector-page.component.ts
+++ src/app/equipos/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs/operators';
+
 import { EquiposService } from '../../services/equipos.service';
 import { Equipo } from '../../interfaces/equipos.interface';
 import { Usuario } from '../../interfaces/usuarios.interface';
@@ -34,16 +36,17 @@ export class SelectorPageComponent implements OnInit {
         this.equipos = teams;
       });
 
-    // Cuando cambie el equipo
+    // Cuando cambie el equipo cambian los usuarios
     this.miFormulario.get('equipo')?.valueChanges
-      .subscribe( equipo => {
-        console.log(equipo);
-        // llamo el servicio
-        this.EquiposService.getUsuariosPorEquipo( equipo )
-          .subscribe( users => {
-            console.log(users);
-            this.usuarios = users;
-          })
+      .pipe(
+        // reiniciar el valor del siguiente select
+        tap( (_) => {
+          this.miFormulario.get('usuario')?.reset('0');
+        }),
+        switchMap( equipo => this.EquiposService.getUsuariosPorEquipo( equipo ) )
+      )
+      .subscribe( users => {
+        this.usuarios = users;
       });
   }
 
